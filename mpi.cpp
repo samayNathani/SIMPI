1d4b7d17a798f684a3e22be524f14377e22ba274
Zachary Hislop <hislopzach@gmail.com>
2020-04-21 14:56:13 -0700

    synch object size is part of simpi initialization

--- mpi.cpp
+++ mpi.cpp
@@ -1,42 +1,53 @@
-#include <sys/mman.h>
-#include <sys/stat.h>       
 #include <fcntl.h>
-#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <sys/time.h>
-#include <vector>
-#include <string>
-#include <unordered_map> 
+#include <unistd.h>
+
+#include <cstring>
 #include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include "simpi.h"
 
-int main(int argc, char* argv[]){
-    
+int main(int argc, char* argv[])
+{
   if (argc != 3) {
-        printf("Usage: ./prog2 prog1 <num workers>");
-        exit(2);
+    printf("Usage: ./prog2 prog1 <num workers>");
+    exit(2);
+  }
+
+  char progname[100];
+  int fd;
+  // strcpy(progname, "./");
+  strcpy(progname, argv[1]);
+
+  int numWorkers = atoi(argv[2]);
+
+  // create shared mem for workers
+  size_t synchObjectSize =
+      sizeof(synch_object) + sizeof(int) * (numWorkers + 1);
+  fd = shm_open(SYNCH_OBJECT_MEM_NAME, O_RDWR | O_CREAT | O_EXCL, 0777);
+  ftruncate(fd, synchObjectSize);
+  synch_object* shared_mem = (synch_object*)mmap(
+      NULL, synchObjectSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+  // initialize ready to zero
+  for (int i = 0; i <= numWorkers; i++) {
+    shared_mem->ready[i] = 0;
+  }
+  shared_mem->par_count = numWorkers;
+
+  for (int i = 0; i < numWorkers; i++) {
+    char workerID[3];
+    std::string size = std::to_string(synchObjectSize);
+    sprintf(workerID, "%d", i);
+    char* args[] = {progname, workerID, &size[0], NULL};
+    if (fork() == 0) {
+      execv(progname, args);
     }
-
- char progname[100];
-    //strcpy(progname, "./");
-    strcpy(progname, argv[1]);
-
-    int numWorkers = atoi(argv[2]);
-    int fd = shm_open(SYNCH_OBJECT_MEM_NAME, O_RDWR | O_CREAT | O_EXCL, 0777);
-	  ftruncate(fd, sizeof(synch_object));
-	  synch_object* shared_mem = (synch_object*)mmap(NULL, sizeof(synch_object), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-    int ready[numWorkers+1];
-    for(int i = 0; i < numWorkers;i++) {
-      ready[i] = 0;
-    } 
-    shared_mem->ready = ready;
-
-    for (int i = 0; i < numWorkers; i++) {
-        char workerID[3];
-        sprintf(workerID, "%d", i);
-        char* args[] = {progname, workerID, NULL};
-        if (fork() == 0) {
-            execv(progname, args);
-        }
-    }
-    exit(0);
+  }
+  exit(0);
 }
--- user.cpp
+++ user.cpp
@@ -1,36 +1,39 @@
 #include "simpi.h"
 #define MATRIX_DIMENSION_XY 10
 
-
-void set_matrix_elem(double* M, int x, int y, double f) {
-    M[x + y * MATRIX_DIMENSION_XY] = f;
+void set_matrix_elem(double* M, int x, int y, double f)
+{
+  M[x + y * MATRIX_DIMENSION_XY] = f;
 }
-void quadratic_matrix_print(double* C) {
+void quadratic_matrix_print(double* C)
+{
+  printf("\n");
+  for (int a = 0; a < MATRIX_DIMENSION_XY; a++) {
     printf("\n");
-    for (int a = 0; a < MATRIX_DIMENSION_XY; a++) {
-        printf("\n");
-        for (int b = 0; b < MATRIX_DIMENSION_XY; b++)
-            printf("%.2f,", C[a + b * MATRIX_DIMENSION_XY]);
-    }
-    printf("\n");
+    for (int b = 0; b < MATRIX_DIMENSION_XY; b++)
+      printf("%.2f,", C[a + b * MATRIX_DIMENSION_XY]);
+  }
+  printf("\n");
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
   printf("im alive\n");
   int par_id = atoi(argv[1]);
   printf("par_id %d\n", par_id);
-  simpi my_simpi(par_id);
+  int synch_size = atoi(argv[2]);
+  printf("synch_size %d\n", synch_size);
+  simpi my_simpi(par_id, synch_size);
   printf("simpi worked\n");
   matrix A(my_simpi, 10, 10);
   printf("matrix worked\n");
   for (int x = 0; x < MATRIX_DIMENSION_XY; x++) {
     for (int y = 0; y < MATRIX_DIMENSION_XY; y++) {
-      A.get(x,y) = (double) x * y;
+      A.get(x, y) = (double)x * y;
     }
   }
   printf("matrix init worked\n");
 
   quadratic_matrix_print(A.arr);
   printf("matrix init worked\n");
-  
 }
