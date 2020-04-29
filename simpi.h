5479f9b5124e6d30ed2c744127bf446e3380aa3e
Zachary Hislop <hislopzach@gmail.com>
2020-04-28 20:33:09 -0700

    move simpi funciton implementations to outside clss declaration

--- simpi.h
+++ simpi.h
@@ -4,9 +4,9 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#include <sstream>
+#include <iostream>
+#include <map>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 #define SYNCH_OBJECT_MEM_NAME "simpi_shared_mem"
@@ -35,7 +35,6 @@ class simpi {
   }
   ~simpi()
   {
-    // clean up shared mem
     for (std::pair<std::string, matrix_metadata> matrix : matrix_info) {
       free_matrix(matrix.second.unique_id);
     }
@@ -43,111 +42,17 @@ class simpi {
   }
   int get_id() { return id; }
 
-  std::pair<std::string, double*> create_matrix(int x, int y)
-  {
-    size_t size = x * y * sizeof(double);
+  std::pair<std::string, double*> create_matrix(int x, int y);
 
-    if (id == 0) {
-      // generate a uniqueid for matrix
-      std::string unique_id = get_shared_mem_name();
-      // create a shared mem object
-      int fd = shm_open(unique_id.c_str(), O_RDWR | O_CREAT | O_EXCL, 0777);
-      ftruncate(fd, size);
-      // allocate matrix
-      double* matrix = (double*)mmap(NULL, sizeof(double) * size,
-                                     PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-      matrix_metadata metadata;
-      metadata.size = size;
-      metadata.file_descriptor = fd;
-      metadata.unique_id = unique_id;
-      metadata.matrix_data = matrix;
-      matrix_info[unique_id] = metadata;
-
-      // write name to synch_object so that other processes can get the uniqie
-      // id
-      synch_info->last_matrix_id = unique_id;
-
-      synch(id, synch_info->par_count, synch_info->ready);
-      std::pair<std::string, double*> pass_back;
-      pass_back = std::make_pair(unique_id, matrix);
-      return pass_back;
-    }
-    else {
-      // wait for id 0 to create the shared memory for matrix
-      synch(id, synch_info->par_count, synch_info->ready);
-      // get the unique id from the synch object
-      std::string unique_id = synch_info->last_matrix_id;
-      // open and allocate the shared memory
-      int fd = shm_open(unique_id.c_str(), O_RDWR, 0777);
-      double* matrix = (double*)mmap(NULL, sizeof(double) * size,
-                                     PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-      // create a metadata object
-      matrix_metadata metadata;
-      metadata.unique_id = unique_id;
-      metadata.file_descriptor = fd;
-      metadata.matrix_data = matrix;
-      matrix_info[unique_id] = metadata;
-      std::pair<std::string, double*> pass_back =
-          std::make_pair(unique_id, matrix);
-      return pass_back;
-    }
-  }
-
-  void free_matrix(std::string unique_id)
-  {
-    // get matrix metadata
-    // close fd, shmunmap, munmap
-    matrix_metadata metadata = matrix_info[unique_id];
-    close(metadata.file_descriptor);
-    shm_unlink(metadata.unique_id.c_str());
-    munmap(metadata.matrix_data, metadata.size);
-  }
+  void free_matrix(std::string unique_id);
 
  private:
   int id;
   synch_object* synch_info;
-  std::string sync_shared_mem_name;  // this shared meory holds: shared mem name
-                                     // for last allocated matrix id, synch
-  std::unordered_map<std::string, matrix_metadata> matrix_info;
-  std::string get_shared_mem_name()
-  {
-    // gets a unique name for each shared memory based on the time that each was
-    // made
-    timeval curTime;
-    gettimeofday(&curTime, NULL);
-    unsigned long micro = curTime.tv_sec * (uint64_t)1000000 + curTime.tv_usec;
-    std::ostringstream ss;
-    ss << "simpi_" << micro;
-    return ss.str();
-  }
-  void synch(int par_id, int par_count, int* ready)
-  {
-    int synchid = ready[par_count] +
-                  1;  // synchid is progressing with each synch - I know, one
-                      // from section 1 and 3 had a similar idea.
-    ready[par_id] = synchid;
-    int breakout = 0;
-    while (1) {
-      breakout = 1;
-      for (int i = 0; i < par_count; i++) {
-        if (ready[i] <
-            synchid)  //"less than" is super important here. Do you know why?
-                      // Because one process could run ahead and
-                      // increment its ready[par_is] var already.
-        {
-          breakout = 0;
-          break;
-        }
-      }
-      if (breakout == 1) {
-        ready[par_count] =
-            synchid;  // and here we increment the additional variable
-        break;
-      }
-    }
-  }
+  std::map<std::string, matrix_metadata> matrix_info;
+  std::string sync_shared_mem_name;
+  std::string get_shared_mem_name();
+  void synch(int par_id, int par_count, int* ready);
 };
 class matrix  // similar stuff for vector
 {
@@ -174,7 +79,7 @@ class matrix  // similar stuff for vector
     // use psimpi for getting rid of the mem and unlink stuff
     psimpi->free_matrix(unique_id);
   }
-  double& get(int x, int y) { return arr[x + x * y]; }
+  double& get(int x, int y) { return arr[x + y * xdim]; }
 };
 
 class vector  // similar stuff for vector
@@ -203,3 +108,97 @@ class vector  // similar stuff for vector
   }
   double& get(int y) { return arr[y]; }
 };
+
+std::string simpi::get_shared_mem_name()
+{
+  // gets a unique name for each shared memory based on the time that each was
+  // made
+  timeval curTime;
+  gettimeofday(&curTime, NULL);
+  unsigned long micro = curTime.tv_sec * (uint64_t)1000000 + curTime.tv_usec;
+  std::string name = "simpi_" + std::to_string(micro);
+  return name;
+}
+
+void simpi::synch(int par_id, int par_count, int* ready)
+{
+  int synchid = ready[par_count] + 1;
+  ready[par_id] = synchid;
+  int breakout = 0;
+  while (1) {
+    breakout = 1;
+    for (int i = 0; i < par_count; i++) {
+      if (ready[i] < synchid) {
+        breakout = 0;
+        break;
+      }
+    }
+    if (breakout == 1) {
+      ready[par_count] = synchid;
+      // and here we increment the additional variable
+      break;
+    }
+  }
+}
+
+std::pair<std::string, double*> simpi::create_matrix(int x, int y)
+{
+  size_t size = x * y * sizeof(double);
+
+  if (id == 0) {
+    // generate a uniqueid for matrix
+    std::string unique_id = get_shared_mem_name();
+    // create a shared mem object
+    int fd = shm_open(unique_id.c_str(), O_RDWR | O_CREAT | O_EXCL, 0777);
+    ftruncate(fd, size);
+    // allocate matrix
+    double* matrix = (double*)mmap(NULL, sizeof(double) * size,
+                                   PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    matrix_metadata metadata;
+    metadata.size = size;
+    metadata.file_descriptor = fd;
+    metadata.unique_id = unique_id;
+    metadata.matrix_data = matrix;
+    matrix_info[unique_id] = metadata;
+
+    // write name to synch_object so that other processes can get the uniqie
+    // id
+    synch_info->last_matrix_id = unique_id;
+
+    synch(id, synch_info->par_count, synch_info->ready);
+    std::pair<std::string, double*> pass_back;
+    pass_back = std::make_pair(unique_id, matrix);
+    return pass_back;
+  }
+  else {
+    // wait for id 0 to create the shared memory for matrix
+    synch(id, synch_info->par_count, synch_info->ready);
+    // get the unique id from the synch object
+    std::string unique_id = synch_info->last_matrix_id;
+    // open and allocate the shared memory
+    int fd = shm_open(unique_id.c_str(), O_RDWR, 0777);
+    double* matrix = (double*)mmap(NULL, sizeof(double) * size,
+                                   PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    // create a metadata object
+    matrix_metadata metadata;
+    metadata.unique_id = unique_id;
+    metadata.file_descriptor = fd;
+    metadata.matrix_data = matrix;
+    matrix_info[unique_id] = metadata;
+    std::pair<std::string, double*> pass_back =
+        std::make_pair(unique_id, matrix);
+    return pass_back;
+  }
+}
+
+void simpi::free_matrix(std::string unique_id)
+{
+  // get matrix metadata
+  // close fd, shmunmap, munmap
+  matrix_metadata metadata = matrix_info[unique_id];
+  close(metadata.file_descriptor);
+  shm_unlink(metadata.unique_id.c_str());
+  munmap(metadata.matrix_data, metadata.size);
+}
