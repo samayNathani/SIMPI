a1614117d1817abda66f0c6d3308c7a3c3c2baee
Zachary Hislop <hislopzach@gmail.com>
2020-04-30 11:05:30 -0700

    seperate x and y dimensions

--- user.cpp
+++ user.cpp
@@ -2,21 +2,18 @@
 #include <string.h>
 
 #include "simpi.h"
-#define MATRIX_DIMENSION_XY 10
+#define MATRIX_DIMENSION_X 2
+#define MATRIX_DIMENSION_Y 3
 
 int par_id;
 
-void set_matrix_elem(double* M, int x, int y, double f)
-{
-  M[x + y * MATRIX_DIMENSION_XY] = f;
-}
 void quadratic_matrix_print(double* C)
 {
   printf("\n");
-  for (int a = 0; a < MATRIX_DIMENSION_XY; a++) {
+  for (int a = 0; a < MATRIX_DIMENSION_Y; a++) {
     printf("\n");
-    for (int b = 0; b < MATRIX_DIMENSION_XY; b++)
-      printf("%.2f,", C[a + b * MATRIX_DIMENSION_XY]);
+    for (int b = 0; b < MATRIX_DIMENSION_X; b++)
+      printf("%.2f,", C[b + a * MATRIX_DIMENSION_X]);
   }
   printf("\n");
 }
@@ -34,11 +31,11 @@ int main(int argc, char* argv[])
   par_id = atoi(argv[1]);
   int synch_size = atoi(argv[2]);
   simpi my_simpi(par_id, synch_size);
-  matrix A(my_simpi, 10, 10);
+  matrix A(my_simpi, MATRIX_DIMENSION_X, MATRIX_DIMENSION_Y);
 
-  for (int x = 0; x < MATRIX_DIMENSION_XY; x++) {
-    for (int y = 0; y < MATRIX_DIMENSION_XY; y++) {
-      A.get(x, y) = (double)x * y;
+  for (int y = 0; y < MATRIX_DIMENSION_Y; y++) {
+    for (int x = 0; x < MATRIX_DIMENSION_X; x++) {
+      A.get(x, y) = (double)x + y;
     }
   }
   quadratic_matrix_print(A.arr);
